91207d66a3d28de8b0448f67b93ce193cb3691ea
KGRozanski <kgrozanski@gmail.com>
2017-03-14 20:15:53 +0100

    v1.4.7

--- index.php
+++ index.php
@@ -13,6 +13,8 @@
 					include(dirname(__FILE__).'/modules/header.html');
 				?>
 				<div class="content">
+					<h1>Po prostu php</h1>
+					<p style="font-size: 24px;">Strona wykonana na przedmiot witryny i aplikacje internetowe.</p>
 				</div>
 				<?php
 					//Załączenie PHPem kodu z pliku html
